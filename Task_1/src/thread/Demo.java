7577acc37890a090d18fdd1ae6d563e7b5585cb1
Konstantin <konstantin.ovcharenko.95@gmail.com>
2020-04-22 22:05:17 +0300

    delete all projects

--- Task_1/src/thread/Demo.java
+++
@@ -1,9 +0,0 @@
-package thread;
-class Demo {
-	public static void main(String[] args) {
-		PrimeNumbers primeNumbers = new PrimeNumbers();
-		primeNumbers.find();
-		Comparison comparison = new Comparison();
-		comparison.compare();
-	}
-}
