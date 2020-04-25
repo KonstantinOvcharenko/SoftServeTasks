b5217f91dac5ef4b1ba953faf6950accd00f792c
Konstantin <konstantin.ovcharenko.95@gmail.com>
2020-04-25 23:04:58 +0300

    Task 2 completed!

--- Task_2/src/annotations/Demo.java
+++ Task_2/src/annotations/Demo.java
@@ -1,6 +1,18 @@
 package annotations;
 
+import java.io.IOException;
+
 // Demonstration
 public class Demo {
-	
+	public static void main(String[] args) {
+		Car car = new Car();
+		try {
+			new Initialization().setFields(car, Car.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(car);
+	}
 }
