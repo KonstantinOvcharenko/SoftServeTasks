53344d6e29baec929b85d532339fa3ee51193242
Konstantin <konstantin.ovcharenko.95@gmail.com>
2020-04-25 20:07:45 +0300

    All subtasks from Task 1 done!

--- Task_1/src/sequence/LongestSequence.java
+++ Task_1/src/sequence/LongestSequence.java
@@ -1,15 +1,74 @@
 package sequence;
 
+import java.util.Scanner;
+
 public class LongestSequence {
 	public static void main(String[] args) {
-	
-		// Не успел уже доделать с запросом нового файла,но обязательно доделаю
-		Searcher searcher = new Searcher("File1.txt");
+
+		Searcher searcher = new Searcher();
 		try {
+			initialize(searcher);
 			searcher.t.join();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+	}
+
+	static void initialize(Searcher searcher) throws InterruptedException {
+		Scanner scan = new Scanner(System.in);
+		int count = 0;
+		boolean repeating = true;
+		do {
+			if (count < 1) {
+				System.out.print("Enter file name to scan: ");
+				searcher.filename = scan.next();
+				count++;
+				searcher.startThread();
+				while (searcher.isFinished == false) {
+					if (searcher.isFinished == true) {
+						break;
+					}
+				}
+				continue;
+			}
+			if (count >= 1) {
+				System.out.print("Do you want to read another file? [Y/N]:");
+				String answer = scan.next();
+				switch (answer) {
+				case "Y":
+					System.out.print("Enter file name to scan: ");
+					searcher.filename = scan.next();
+					searcher.startThread();
+					break;
+				case "y":
+					System.out.print("Enter file name to scan: ");
+					searcher.filename = scan.next();
+					searcher.startThread();
+					break;
+				case "N":
+					scan.close();
+					repeating = false;
+					searcher.terminate();
+					System.out.println("Program finished!");
+					break;
+				case "n":
+					scan.close();
+					repeating = false;
+					searcher.terminate();
+					System.out.println("Program finished!");
+					break;
+				}
+				while (searcher.isFinished == false) {
+					if (searcher.isFinished == true) {
+						break;
+					}
+					if (repeating == false) {
+						break;
+					}
+				}
+			}
+		} while (repeating);
 	}
 }
--- Task_1/src/sequence/Searcher.java
+++ Task_1/src/sequence/Searcher.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Searcher implements Runnable {
 	Thread t;
@@ -13,54 +14,58 @@ public class Searcher implements Runnable {
 	String filename = null;
 	byte[] bytes;
 	LinkedList<Sequence> sequenceList = new LinkedList<Sequence>();
+	boolean isRepeat = true;
+	boolean isFinished = false;
 
-	public Searcher(String filename) {
+	public Searcher() {
 		suspendFlag = true;
-		this.filename = filename;
 		t = new Thread(this, "sequence finder");
 		t.start();
 	}
 
 	public void run() {
-		try {
-			bytes = Files.readAllBytes(Paths.get("src/sequence/" + filename));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		// Searching algorhythm
-		int length = 2;
-		while (length < bytes.length) {
-			for (int i = 0; i < bytes.length - length + 1; i++) {
-
-				Sequence sq = new Sequence();
-				for (int s = i; s < i + length; s++) {
-					sq.addByte(bytes[s]);
+		do {
+			isFinished = false;
+			synchronized (this) {
+				while (suspendFlag) {
+					try {
+						wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
-
-				if (sq.isUnique(sequenceList)) {
-					sq.check(bytes, length);
-					sequenceList.add(sq);
-					System.out.println(sq);
-				}
-
 			}
-			length++;
-		}
-		System.out.println();
-		findLongest(sequenceList);
 
+			if (isRepeat == false) {
+				continue;
+			}
+
+			try {
+				bytes = Files.readAllBytes(Paths.get("src/sequence/" + filename + ".txt"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			searchFunc();
+
+			System.out.println();
+			findLongest(sequenceList);
+			isFinished = true;
+			clearData();
+			stopThread();
+		} while (isRepeat);
 	}
 
-	// synchronized void stopThread() {
-	// suspendFlag = true;
-	// }
+	synchronized void stopThread() {
+		suspendFlag = true;
+	}
 
-	// synchronized void startThread() {
-	// suspendFlag = false;
-	// notify();
-	// }
+	synchronized void startThread() {
+		suspendFlag = false;
+		notify();
+	}
 
 	public void printSequences() {
 		for (Sequence s : sequenceList) {
@@ -80,5 +85,37 @@ public class Searcher implements Runnable {
 			}
 		}
 		System.out.println(longest);
+	}
+
+	void searchFunc() {
+		// Searching algorhythm
+		int length = 2;
+		while (length < bytes.length) {
+			for (int i = 0; i < bytes.length - length + 1; i++) {
+
+				Sequence sq = new Sequence();
+				for (int s = i; s < i + length; s++) {
+					sq.addByte(bytes[s]);
+				}
+
+				if (sq.isUnique(sequenceList)) {
+					sq.check(bytes, length);
+					sequenceList.add(sq);
+					System.out.println(sq);
+				}
+
+			}
+			length++;
+		}
+	}
+
+	public void terminate() {
+		isRepeat = false;
+		startThread();
+	}
+
+	public void clearData() {
+		sequenceList.clear();
+		bytes = null;
 	}
 }
